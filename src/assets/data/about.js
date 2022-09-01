15b2005c14a4685f8749c8b3e4fe782dff149bf3
Sandeep Sthapit <sandeepsthapit@gmail.com>
2022-09-01 14:14:36 -0400

    Update about.js

--- src/assets/data/about.js
+++ src/assets/data/about.js
@@ -6,7 +6,7 @@ export const introductionData = {
     texts: [
         'Hello, I am Sandeep.',
         'I am Computer Science PhD student at North Carolina State University.',
-        'My research interests are in Human Computer Interaction, Interactive learning environments, Computer Games and User Experience. I have a unique combination of design, development, and research experience that enables me to tackle a diverse set of problems.',
+        'My research interests are in Human Computer Interaction, Interactive learning environments, and Education. I have a unique combination of design, development, and research experience that enables me to tackle a diverse set of problems.',
         'I am currently working as Graduate Teaching Assistant for Human Computer Interaction.'
     ]
 }
